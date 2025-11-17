ca2a9509596dd5399811b6f323d5135161f9de0b
rahmatsaputra-my-id <rahmatsaputra.my.id@gmail.com>
2025-11-17 17:49:45 +0700

    - update touchableopacity

--- src/components/TouchableOpacity/TouchableOpacity.component.tsx
+++ src/components/TouchableOpacity/TouchableOpacity.component.tsx
@@ -5,16 +5,24 @@ const TouchableOpacity = ({
   children,
   onPress,
   style,
+  disabled = false,
   ...props
-}: ITouchableOpacityProps): JSX.Element | null => {
-  const handleOnPress = () => {
-    if (onPress) onPress();
+}: ITouchableOpacityProps & {disabled?: boolean}): JSX.Element | null => {
+  const handleOnPress = (event: React.MouseEvent) => {
+    if (!disabled && onPress) {
+      onPress(event);
+    }
   };
 
   return (
     <div
       onClick={handleOnPress}
-      style={{...styles.content, ...style}}
+      style={{
+        ...styles.content,
+        ...style,
+        opacity: disabled ? 0.5 : 1,
+        pointerEvents: disabled ? 'none' : 'auto',
+      }}
       {...props}>
       {children}
     </div>
