85dfa3ccbb5a5b5184ebf7f9fe4cb7e233e1e03e
rahmatsaputra-my-id <rahmatsaputra.my.id@gmail.com>
2026-03-22 22:32:59 +0700

    feat: update color

--- src/components/Switch/Switch.styles.ts
+++ src/components/Switch/Switch.styles.ts
@@ -1,4 +1,4 @@
-import { Colors } from '../../constants/Colors';
+import { colors } from '@rahmatsaputra-my-id/global-assets';
 
 export const styles = {
   containerWithBorder: {
@@ -7,10 +7,10 @@ export const styles = {
     justifyContent: 'space-between',
     marginTop: 16,
     marginBottom: 16,
-    backgroundColor: Colors.white,
+    backgroundColor: colors.white,
     padding: 8,
     borderRadius: 4,
-    border: `0.5px solid ${Colors.grey80}`,
+    border: `0.5px solid ${colors.lightGray}`,
   },
   container: {
     display: 'flex',
